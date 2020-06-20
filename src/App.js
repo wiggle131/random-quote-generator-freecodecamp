e2e032292de95dc096ec618b69f6085cf2ec9451
wiggle131 <jhosiecharlesespantaleon@gmail.com>
2020-06-20 18:56:28 +0800

    Using Hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { random } from "lodash";
 import QuoteMachine from "./components/QuoteMachine";
@@ -13,67 +13,68 @@ const styles = {
     display: "flex",
   },
 };
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quote: [],
-      selectedQuoteIndex: null,
-    };
 
-    this.quoteIndex = this.generateQuoteIndex.bind(this);
-    this.nextQuote = this.nextQuote.bind(this);
-  }
+function App({ classes }) {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     quote: [],
+  //     selectedQuoteIndex: null,
+  //   };
 
-  componentDidMount() {
-    fetch(
-      "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json"
-    )
-      .then((data) => data.json())
-      .then((d) => this.setState({ quote: d }, this.nextQuote));
-  }
+  //   this.quoteIndex = this.generateQuoteIndex.bind(this);
+  //   this.nextQuote = this.nextQuote.bind(this);
+  // }
 
-  get selectedQuote() {
-    if (
-      !this.state.quote.length ||
-      !Number.isInteger(this.state.selectedQuoteIndex)
-    ) {
+  const [quote, setQuote] = useState([]);
+  const [selectedQuoteIndex, setSelectedQuoteIndex] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetch(
+        "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json"
+      );
+      const quotes = await data.json();
+      setQuote(quotes);
+      setSelectedQuoteIndex(nextQuote());
+    }
+    fetchData();
+  });
+
+  function getSelectedQuote() {
+    if (quote.length || !Number.isInteger(selectedQuoteIndex)) {
       return undefined;
     }
-    return this.state.quote[this.state.selectedQuoteIndex];
+    return quote[selectedQuoteIndex];
   }
 
-  generateQuoteIndex() {
-    if (!this.state.quote.length) {
+  function generateQuoteIndex() {
+    if (quote.length) {
       return;
     }
-    return random(0, this.state.quote.length - 1);
+    return random(0, quote.length - 1);
   }
 
-  nextQuote() {
-    this.setState({ selectedQuoteIndex: this.generateQuoteIndex() });
+  function nextQuote() {
+    setSelectedQuoteIndex(generateQuoteIndex());
   }
-
-  render() {
-    console.log(this.state.selectedQuoteIndex);
-    return (
-      <Grid
-        className={this.props.classes.container}
-        justify="center"
-        id="quote-box"
-        container
-      >
-        <Grid xs={11} lg={8} item>
-          {this.selectedQuote ? (
-            <QuoteMachine
-              selectedQuote={this.selectedQuote}
-              nextQuote={this.nextQuote}
-            />
-          ) : null}
-        </Grid>
+  return (
+    <Grid
+      className={classes.container}
+      justify="center"
+      id="quote-box"
+      container
+    >
+      <Grid xs={11} lg={8} item>
+        {getSelectedQuote ? (
+          <QuoteMachine
+            selectedQuote={getSelectedQuote}
+            nextQuote={nextQuote}
+          />
+        ) : null}
       </Grid>
-    );
-  }
+    </Grid>
+  );
 }
 
 export default withStyles(styles)(App);
